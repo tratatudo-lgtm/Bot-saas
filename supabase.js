a8b63926f8bb8e2af1ad4a5ae08a4be537817eca
tratatudo-lgtm <u1749243275@gmail.com>
2026-02-25 21:47:00 +0000

    Atualizar o supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
-const { createClient } = require('@supabase/supabase-js')
+import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 )
 
-module.exports = supabase
+export default supabase
